014ddd321ee6bd23e09299895ca33e88d85776a9
iuri <iuricube@gmail.com>
2020-12-09 14:57:20 +0100

    added Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //
+    }
+}
