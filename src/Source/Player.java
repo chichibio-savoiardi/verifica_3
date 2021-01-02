ee4f94f3c4058bbdba47b98690e565ec2c4833da
iuri <iuricube@gmail.com>
2021-01-02 16:35:14 +0100

    Update Client class
    finished initPlayers()
    started turn() and isDone()

--- src/Source/Player.java
+++ src/Source/Player.java
@@ -15,12 +15,21 @@ public abstract class Player {
 
     public String attacca(Player player) {
         int dmg = 15;
-        if (stamina < dmg) return "Non hai abbastanza stamina";
-        stamina -= 20;
+        int stm = 20;
+        if (stamina < stm) return "Non hai abbastanza stamina";
+        stamina -= stm;
         if (player.difesa > dmg) dmg = 0;
         else dmg -= player.difesa;
         player.puntiVita -= dmg;
         return player.nome + " e' stato attaccato con un attacco normale";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPuntiVita() {
+        return puntiVita;
     }
 
     @Override
